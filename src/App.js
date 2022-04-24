f5dd6e23f6e910d1925328ea13e4d2fb31b584a8
Hasireddy <malikireddy.uma@gmail.com>
2022-04-24 13:13:06 +0200

    fetch part completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { useState, useEffect } from 'react';
+import "./App.css";
+const article = (data) => {
+  const result = data.results;
+  // alert(data.results);
+  return result.map((iteration) => {
+    return <article>
+      <div>
+        <p>Name : {iteration.name.title}</p>
+        <p>Firstname : {iteration.name.first}</p>
+        <p>Lastname : {iteration.name.last}</p>
+        <img src={iteration.picture.medium} alt="Employee" />
+      </div>
+    </article>
+  }
+  )
+}
 function App() {
+  const url = "https://randomuser.me/api/?results=15&&gender=";
+  const [input, setInput] = useState();
+  const [searchurl, setSearchurl] = useState();
+  const [fetchdata, setFetchdata] = useState();
+  const handleChange = (item) => setInput(item.target.value);
+  const handleSubmit = (click) => {
+    click.preventDefault();
+    setSearchurl(url + input)
+  };
+  useEffect(() => {
+    fetch(searchurl)
+      .then(response => response.json())
+      .then(jsonobj => setFetchdata(article(jsonobj)))
+  },
+    [searchurl])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <input type="text" value={input} onChange={handleChange} />
+        <button type="submit" onClick={handleSubmit}>Submit</button>
+        <main>{fetchdata}</main>
+      </div>
+    </>
   );
 }
 
